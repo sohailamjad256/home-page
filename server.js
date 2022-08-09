54602e55d511c68c9518d8885aa41eb6e7a5d90c
sohailamjad256 <53993286+sohailamjad256@users.noreply.github.com>
2022-08-09 12:47:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/home-page'));
+app.use(express.static(__dirname + '/dist/sohail-first-app'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/home-page/index.html'));
+res.sendFile(path.join(__dirname+'/dist/sohail-first-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
